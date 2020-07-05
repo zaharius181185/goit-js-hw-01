eb5702cde1e81a07593dd0b08d7984ad1bc3699b
zaharius <zaharius.ltd@gmail.com>
2020-07-06 01:03:25 +0300

    end task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
+const name = 'Генератор защитного поля'
 
+let price = '1000'
 
+console.log ('Выбран «', name, '» цена за штуку', price);
 
+price = '2000'
+
+console.log ('Выбран «', name, '» цена за штуку', price);
